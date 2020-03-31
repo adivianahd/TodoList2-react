b7b82cfc2d5ea220498087f65d631e4e14f97c63
adivianahd <adivianahd07@gmail.com>
2020-03-31 16:25:51 -0300

    add update item

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function Todolist() {
   const [data, setData] = useState([]);
-  let [newData, setNewData] = useState("");
+  const [newData, setNewData] = useState("");
   return (
-   <div>
-     <input type="text" value={newData} onChange={(e) => {
-       setNewData(e.target.value);
-     }}></input>
-     <button onClick={()=>{
-       if(newData === "") return;
-        setData([newData, ...data]);
+    <div>
+      <input type="text" value={newData} onChange={(e) => {
+        setNewData(e.target.value);
+      }} />
+      <button onClick={() => {
+        if (newData === "") return;
+        setData([{ str: newData, boolean: true }, ...data]);
         setNewData("");
-     }}>Add</button>
-     <div>
-     <List data={data}/>
-     </div>
-   </div>
+      }}>Add</button>
+      <div>
+        <List data={data} onUpdate={(pos, val) => {
+          
+          const _data = data.concat();
+          _data[pos] = val;
+          setData(_data);
+        }}/>
+      </div>
+    </div>
   );
 }
 
-function List(props){
-  return(
+function List(props) {
+  return (
     <ul>
-      {props.data.map((e, i)=>
-      <li key={i}>{e}
-        <input type="checkbox" checked={true} />    
-      </li>
+      {props.data.map((e, i) =>
+        <li key={i}>{e.str}
+          <input
+            type="checkbox"
+            checked={e.boolean}
+            onChange={chkbox => {
+              props.onUpdate(i, { str: e.str, boolean: chkbox.target.checked });
+            }}
+          />
+        </li>
       )}
     </ul>
   )
